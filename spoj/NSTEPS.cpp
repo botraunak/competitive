2a573f340aa39521b6582e7262b141a758c4a6db
Raunak Sett <sett.raunak@gmail.com>
2015-08-24 21:58:45 +0530

    done second Problem

---
+++ spoj/NSTEPS.cpp
@@ -0,0 +1,47 @@
+/*
+
+ SPOJ PROBLEM : NSTEPS - Number Steps
+ URL : http://www.spoj.com/problems/NSTEPS/
+
+*/
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	int t; // testcases
+	int x,y,result; // cordinates;
+	bool found;
+	cin>>t;
+	while(t--){
+		found = false;
+		cin>>x>>y;
+		if(x%2==0){
+			// if x is even and y is even too
+			if(y%2 == 0 && y >= x-2 && y <= x){
+				result = x+y;
+				found = true;
+			}
+			else{
+				found = false;
+			}
+		}
+		else{
+			// x is odd
+			if(y%2 != 0 && y >= x-2 && y <= x){
+				result = x+y -1;
+				found = true;
+			}
+			else{
+				found = false;
+			}
+		}
+		if (found)
+			cout<<result<<endl;
+		else
+			cout<<"No Number"<<endl;
+
+	}
+	return 0;
+}
